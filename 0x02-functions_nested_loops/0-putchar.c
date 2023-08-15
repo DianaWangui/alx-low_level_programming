2c3b8b774ffdbf47f5b76d1fa141fb075006f216
DianaWangui <dianawanguikt048@gmail.com>
2023-08-15 02:27:48 -0700

    a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+ * main -prints out _putchar
+ *
+ * Return: On success 1.
+ */
+int main(void)
+{
+	int _putchar(char c);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
