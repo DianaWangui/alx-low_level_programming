dc8a90cd982c79c5bfaac9eb18500356b7c3905a
DianaWangui <dianawanguikt048@gmail.com>
2023-08-17 23:39:13 +0300

    another correction

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * positive_or_negative - This function prints random number
- * stores it in the variable i,
+ * stores it in the variable i
  * and then prints whether the number is positive, negative or zero.
+ * @i: the number passed to the fuction 
  * Return: :0 (Success)
  */
 void positive_or_negative(int i)
