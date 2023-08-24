662bf67e9111be69167e7f2e8686b666613e99a7
DianaWangui <dianawanguikt048@gmail.com>
2023-08-24 12:56:04 +0300

    Capitalizing each word in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,50 +1,34 @@
 #include "main.h"
 /**
- * is_separator - checks if a character is a word separator
- * @c: The character to check
- * Return: true if the character is a separator, false otherwise
- */
-int is_separator(char c)
-{
-	char separators[] = "\t\n,;.!?\"(){}";
-
-	int i;
-
-	for (i = 0; separators[i] != '\0'; i++)
-	{
-		if (c == separators[i])
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-/**
- * cap_string - Capitalizes all words of the string
- * @str: The input string
- * Return: A pointe to the modified string
+ * cap_string - capitalize all words of a string
+ * @str: the string to be capitalized
+ *
+ * Return: a pointer to the changed string
  */
 char *cap_string(char *str)
 {
-	int new_word = 1;
+	int index = 0;
 
-	char *ptr = str;
-
-	while (*ptr != '\0')
+	while (str[index])
 	{
-		if (is_separator(*ptr))
-		{
-			new_word = 1;
-		}
-		else if (new_word)
-		{
-			if (*ptr >= 'a' && *ptr <= 'z')
-			{
-				*ptr = *ptr -('a' - 'A');
-			}
-			new_word = 0;
-		}
-		ptr++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index ==  0)
+			str[index] -= 32;
+		index++;
 	}
 	return (str);
 }
