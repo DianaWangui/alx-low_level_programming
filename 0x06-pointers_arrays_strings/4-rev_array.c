e8ab99f2abc02264da2fe6a577e008f0da165a8b
DianaWangui <dianawanguikt048@gmail.com>
2023-08-24 11:13:25 +0300

    Correcting betty errors

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,11 +8,12 @@ void reverse_array(int *a, int n)
 {
 	int start = 0;
 
-	int end = n-1;
+	int end = n - 1;
 
 	while (start < end)
 	{
 		int temp = a[start];
+
 		a[start] = a[end];
 		a[end] = temp;
 		start++;
