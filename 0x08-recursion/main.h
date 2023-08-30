8d235f359c8c9101201b5e4a020ba87e4d0da5df
DianaWangui <dianawanguikt048@gmail.com>
2023-08-30 16:12:19 +0300

    a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,5 +15,7 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int actual_prime(int n, int i);
 int actual_sqrt_recursion(int n, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 #endif /* MAIN_H */
