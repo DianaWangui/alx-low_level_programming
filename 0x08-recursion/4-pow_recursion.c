713870479b10af80d906d6816dbbf8f33c08b9a6
DianaWangui <dianawanguikt048@gmail.com>
2023-08-30 14:41:39 +0300

    A recursive function that returns the power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - A function that returns the value of x
+ * raise to the power of y
+ * @x: The value
+ * @y: The power the value is rased to
+ * Return: The value of x to power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x , y -1));
+}
