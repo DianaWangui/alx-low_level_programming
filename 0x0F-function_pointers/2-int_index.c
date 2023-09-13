5bf6b5f7faafeaaedbf38ad8acbe986c79658fcb
DianaWangui <dianawanguikt048@gmail.com>
2023-09-13 15:53:42 +0300

    Correcting code run error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array && cmp)
 	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]) != 0)
+				return (i);
 		}
 	}
 	return (-1);
