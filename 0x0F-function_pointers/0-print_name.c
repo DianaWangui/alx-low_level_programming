0136329f7adc414c3003c319b1032424e527598b
DianaWangui <dianawanguikt048@gmail.com>
2023-09-13 13:36:28 +0300

    Correcting code failing checker error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,10 @@
  * print_name - a function that prints a name
  * @name: name to be printed
  * @f: pointer to a function
- * @char: pointer to a character
- *
- * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
 }
