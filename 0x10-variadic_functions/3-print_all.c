16ccd3bf4922bc759e916a6c421227d277e9abbc
DianaWangui <dianawanguikt048@gmail.com>
2023-09-14 20:26:55 +0300

    Correcting one code fail error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,40 +9,44 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 
+	int i = 0;
+
 	char *str;
 
 	char *s = "";
 
-	const char *fmt_iter = format;
-
 	va_start(args, format);
 
-	while (fmt_iter && *fmt_iter)
+	if (format)
 	{
-		switch (*fmt_iter)
+		while (format[i])
 		{
-			case 'c':
-				printf("%s%c", s, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", s, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", s, va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "nil";
-				printf("%s%s", s, str);
-				break;
-			default:
-				fmt_iter++;
-				continue;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", s, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", s, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", s, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", s, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			s = ", ";
+			i++;
 		}
-		s = ", ";
-		fmt_iter++;
 	}
-	va_end(args);
+
 	printf("\n");
+	va_end(args);
 }
