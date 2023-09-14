7ffddf92ef38d383e93b7d86b04e3ad2d6b7be83
DianaWangui <dianawanguikt048@gmail.com>
2023-09-14 16:24:43 +0300

    function that returns the sum of all its parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - variable function that gives the sum of all its parameters
+ * @n: the number of arguments
+ * @...: The variable arguments of int type
+ *
+ * Return: sum of the parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+
+	unsigned int i;
+
+	va_list args;
+	va_start(args, n);
+	if (n == 0)
+	{
+		va_end(args);
+		return (0);
+	}
+	sum = 0;
+	for (i = 0; i< n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+ALX Project on Variadic function. This is the task in this order
+0. Write a function that returns the sum of all its parameters.
+1. Write a function that prints numbers, followed by a new line.
+2. Write a function that prints strings, followed by a new line.
+4. Write a function that prints anything.
