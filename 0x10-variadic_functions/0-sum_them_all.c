6c329d8159dac336e2e87a87157183eabbf175e9
DianaWangui <dianawanguikt048@gmail.com>
2023-09-14 16:31:20 +0300

    correcting betty error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_list args;
+
 	va_start(args, n);
 	if (n == 0)
 	{
@@ -21,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 	sum = 0;
-	for (i = 0; i< n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
