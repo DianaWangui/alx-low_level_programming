afd2bb95613b90356a72e3893bd00d190eed168a
DianaWangui <dianawanguikt048@gmail.com>
2023-10-02 21:50:12 +0300

    betty error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,51 +5,30 @@
  * @letters: number of letters to be printed
  * @filename: pointer to the file
  * Return: actual number of letters read and printed
- * 0 when file cannot be opened file name is NULL
- * and write fails
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-
-	ssize_t bytes_read;
-
-	ssize_t bytes_written;
+	int fd, bytes_read, bytes_written;
 
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * (letters + 1));
-
-	if (buffer == NULL)
+	if (filename == NULL || buffer == NULL)
 	{
-		close(fd);
+		free(buffer);
 		return (0);
 	}
 	bytes_read = read(fd, buffer, letters);
-
 	if (bytes_read == -1)
-	{
-		close(fd);
-		free(buffer);
 		return (0);
-	}
 	buffer[bytes_read] = '\0';
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
 	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd);
-		free(buffer);
 		return (0);
-	}
 	close(fd);
 	free(buffer);
-
 	return (bytes_read);
 }
