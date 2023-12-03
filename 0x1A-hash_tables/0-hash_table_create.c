4001232bf6c88477321fcc2559f3ae52b2ff43d4
DianaWangui <dianawanguikt048@gmail.com>
2023-12-03 12:39:01 +0300

    betty error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,8 @@
 #include "hash_tables.h"
 /**
  * hash_table_create - a function that creates a hash table
- * @size: the size of the array
- * @Return: returns a pointer to hash table
- * error: return NULL
+ * @size: the size of hash table to create
+ * Return: returns a pointer to hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
