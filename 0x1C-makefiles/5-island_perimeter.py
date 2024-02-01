3e0720690929afd81d618467cb10bc581cb739c9
DianaWangui <dianawanguikt048@gmail.com>
2024-02-01 11:25:24 +0300

    task 5, island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+  per = 0
+  for i in range(len(grid)):
+    for j in range(len(grid[0])):
+      if grid[i][j] == 1:
+        if i == 0 or grid[i - 1][j] == 0:
+          per += 1
+        if i == len(grid) - 1 or grid[i + 1][j] == 0:
+          per += 1
+        if j == 0 or grid[i][j - 1] == 0:
+          per += 1
+        if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+          per += 1
+  return per
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
