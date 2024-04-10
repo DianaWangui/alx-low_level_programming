de0286047aee2d84ab63727e357d8576e4eaec38
DianaWangui <dianawanguikt048@gmail.com>
2024-04-10 11:58:44 +0300

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef __SEARCH_ALGOS_H__
-#define __SEARCH_ALGOS_H__
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,4 +8,4 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
-#endif
+#endif /* _SEARCH_ALGOS_H_ */
