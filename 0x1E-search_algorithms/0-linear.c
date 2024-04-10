bbab93c79169b1cc085abc1d244e1e953405198b
DianaWangui <dianawanguikt048@gmail.com>
2024-04-10 11:53:10 +0300

    remove redudance if statement to check null array

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,9 +10,6 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i;
 
-    if (array == NULL)
-        return (-1);
-
     for (i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
