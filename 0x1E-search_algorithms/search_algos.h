b223e8a300e1408592f2574396b4ba01a9f050c0
DianaWangui <dianawanguikt048@gmail.com>
2024-04-10 10:36:20 +0300

    update header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef __SEARCH_ALGOS_H__
+#define __SEARCH_ALGOS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
