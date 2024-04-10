948132373cfcb1ccf64594048698ff2d55b616b0
DianaWangui <dianawanguikt048@gmail.com>
2024-04-10 11:22:07 +0300

    uppdate for correct output error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = left; i < right; i++)
 		{
-			printf("%d ,", array[i]);
+			printf("%d, ", array[i]);
 		}
 		printf("%d\n", array[i]);
 		mid = (left + right) / 2;
