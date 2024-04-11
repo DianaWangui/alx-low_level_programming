73ad8124692217a0bd74c97a12af36edeac86abd
DianaWangui <dianawanguikt048@gmail.com>
2024-04-12 00:47:04 +0300

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,13 @@
 #include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+/*int binary_search(int *array, size_t size, int value);*/
+int binary_search(int *array, size_t left, size_t right, int value);
+int exponential_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+ 
+
+
 
 #endif /* _SEARCH_ALGOS_H_ */
